c5249fb3e15a9545690e1c9a23d4a5279c617def
Sunan Tajwar <tajwarsunan@gmail.com>
2021-09-23 21:42:35 -0400

    Initial Commit

---
+++ Flixster_Codepath/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flixster_Codepath
+//
+//  Created by Sunan Tajwar on 9/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
