a6e570dc14039532b716cca3a41417f2ab32c602
Sunan Tajwar <tajwarsunan@gmail.com>
2021-09-29 22:46:09 -0400

    Finished Flixster Part 2

---
+++ Flixster_Codepath/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixster_Codepath
+//
+//  Created by Sunan Tajwar on 9/29/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
--- Flixster_Codepath/MoviesViewController.swift
+++ Flixster_Codepath/MoviesViewController.swift
@@ -68,6 +68,27 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         return cell
     }
     
+    //MARK: - Navigation
+    // In a storyboard-based application, you will often want to do a little
+    // preperation before navigation
+    override func prepare(for segue:
+        UIStoryboardSegue, sender: Any?) {
+        // get the new view controller using segue.destination
+        // pass the selected object to the new view controller
+        
+        print("Loading up details screen")
+        
+        // find the selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        // Pass the selected movie to the details view controller
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
 
 }
 
